dd4dc013604bbe7a10529892970e7033b76815dd
Jonathan Farias <jfarias@dssayhello.com>
2023-10-11 19:12:38 -0600

    Segunda Preentrega, se realiza ajustes opara poner funciones y arrays

--- js/main.js
+++ js/main.js
@@ -1,65 +1,70 @@
-
-let montoInversion=0;
-let montoinversionInicial=0;
-let gananciaInversion=0;
-let montoAcumulado=0;
-let retornoCapital=0;
-let mesesInversion=0;
+let montoInversion = 0;
+let montoInversionInicial = 0;
+let gananciaInversion = 0;
+let montoAcumulado = 0;
+let retornoCapital = 0;
+let mesesInversion = 0;
 let nombreUsuario;
-let interesCompuesto=false;
+let interesCompuesto = false;
 let tasaInteresFija;
-const impuestos=0.12;
+const impuestos = 0.12;
+let inversiones = [];
 
-alert("Bienvenido al inicio de Portal de Inversiones");
-nombreUsuario= prompt("Hola Escribe tu nombre")
-montoInversion= parseFloat(  prompt("Gracias " + nombreUsuario + " Introduze tu monto a invertir en dolares"))
-mesesInversion= parseInt(prompt("Gracias, para continuar coloque el numero de meses de inversion, solo en numeros puede ser 6 (tasa 2%), 12 (tasa 2.5%), 24 (tasa 4.0%) meses, la tasa mostrada es mensual"));
-let compuesto= prompt("Gracias, nuestro modelo de inversion permite realizar interes compuesto, lo cual nos permite tomar el interes generado el mes pasado y sumarlo a capital para invertir en el siguiente mes, desea que apliquemos interes compuesto?, solo escriba si o no")
-alert("Trabajamos en su tabla de amortizacion un momento por favor.")
-montoinversionInicial=montoInversion;
-console.log("monto inversion "+ montoInversion);
-console.log("meses inversion "+ mesesInversion);
-console.log("compuesto "+ compuesto);
-
-if(mesesInversion==6){
-    tasaInteresFija=0.02
-} else if(mesesInversion==12){
-    tasaInteresFija=0.025
-} else if(mesesInversion==24){
-    tasaInteresFija=0.04
+function calcularGananciaMensual(monto, tasa) {
+    return monto * tasa;
 }
 
-if(compuesto=="si"){
-    interesCompuesto=true
+function calcularInversionesCompuestas(monto, meses, tasa) {
+    let inversiones = [];
+    for (let i = 0; i < meses; i++) {
+        gananciaInversion = calcularGananciaMensual(monto, tasa);
+        monto += gananciaInversion;
+        montoAcumulado += gananciaInversion;
+        retornoCapital = montoInversionInicial + montoAcumulado;
+        inversiones.push({ Mes: i + 1, GananciaMensual: gananciaInversion, MontoAcumulado: montoAcumulado, RetornoCapital: retornoCapital });
+    }
+    return inversiones;
 }
 
-console.log("Interes Compuesto: "+interesCompuesto)
-
-if(interesCompuesto==false){
-
-    montoAcumulado=montoInversion*tasaInteresFija*mesesInversion;
-    console.log(montoAcumulado);
-    retornoCapital=montoInversion+montoAcumulado;
-
-}else{
-
-   
-    for(let i=0;i<mesesInversion;i++){
-
-    montoInversion=montoInversion+gananciaInversion;
-    console.log(montoInversion);
-    gananciaInversion=(montoInversion*tasaInteresFija);
-    console.log("Monto ganancia con Interes Compuesto " +gananciaInversion);
-    montoAcumulado=montoAcumulado+gananciaInversion
-    console.log("Monto Acumulado de Ganancias "+montoAcumulado)
-    retornoCapital=montoinversionInicial+montoAcumulado;
-    console.log("Monto retorno "+ retornoCapital);
- 
-} 
-
-
+function obtenerInversionesConGananciaSuperior(inversiones, gananciaMinima) {
+    return inversiones.filter(inversion => inversion.GananciaMensual > gananciaMinima);
 }
 
-alert("Estimado "+nombreUsuario+"       Su tabla de amortizacion esta lista: en base a lo valores su inversion inicial tendra un rendimiento al final del periodo de " +montoAcumulado +" USD, Recibiendo al final del periodo de " +mesesInversion +" meses un monto total de "+retornoCapital+" USD")
+alert("Bienvenido al inicio del Portal de Inversiones");
+nombreUsuario = prompt("Hola, escribe tu nombre");
+montoInversion = parseFloat(prompt("Gracias " + nombreUsuario + " Introduce tu monto a invertir en dólares"));
+mesesInversion = parseInt(prompt("Gracias, para continuar coloca el número de meses de inversión. Puede ser 6 (tasa 2%), 12 (tasa 2.5%), o 24 (tasa 4.0%) meses."));
+let compuesto = prompt("Gracias, nuestro modelo de inversión permite el interés compuesto. ¿Deseas aplicar interés compuesto? Escribe 'si' o 'no'");
+alert("Trabajamos en tu tabla de amortización. Un momento, por favor.");
+montoInversionInicial = montoInversion;
+
+if (mesesInversion == 6) {
+    tasaInteresFija = 0.02;
+} else if (mesesInversion == 12) {
+    tasaInteresFija = 0.025;
+} else if (mesesInversion == 24) {
+    tasaInteresFija = 0.04;
+}
+
+if (compuesto === "si") {
+    interesCompuesto = true;
+}
+
+console.log("Interés Compuesto: " + interesCompuesto);
+
+if (!interesCompuesto) {
+    montoAcumulado = montoInversion * tasaInteresFija * mesesInversion;
+    retornoCapital = montoInversion + montoAcumulado;
+} else {
+    inversiones = calcularInversionesCompuestas(montoInversion, mesesInversion, tasaInteresFija);
+}
+
+alert("Estimado " + nombreUsuario + ", tu tabla de amortización está lista:\n\n" +
+    "Según los valores iniciales, al final del período tendrás un rendimiento de " + montoAcumulado.toFixed(2) + " USD.\n" +
+    "Al final de " + mesesInversion + " meses, tendrás un monto total de " + retornoCapital.toFixed(2) + " USD");
 
 
+const gananciaMinima = 100; 
+const inversionesConGananciaSuperior = obtenerInversionesConGananciaSuperior(inversiones, gananciaMinima);
+console.log("Inversiones con ganancia superior a " + gananciaMinima + ":");
+console.log(inversionesConGananciaSuperior);
